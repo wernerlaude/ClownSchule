34488b97e4c70418d4a4d5a1f5e2ceb2257b6bdf
wernerlaude <mail@laude.dev>
2025-10-28 12:56:25 +0100

    Inhalte eingefügt

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def inline_svg(filename, options = {})
+    file_path = Rails.root.join("app", "assets", "images", filename)
+
+    if File.exist?(file_path)
+      svg = File.read(file_path)
+
+      # Optional: CSS-Klassen hinzufügen
+      if options[:class].present?
+        svg.sub("<svg", "<svg class='#{options[:class]}'").html_safe
+      else
+        svg.html_safe
+      end
+    else
+      "<!-- SVG #{filename} not found -->"
+    end
+  end
 end
